93fb00bda2e4bfb9560536a587d03e7e1105ed09
vortexnull <53948403+vortexnull@users.noreply.github.com>
2024-04-28 16:38:47 +0530

    Delete Assignment2/filesystem directory

--- Assignment2/filesystem/bitmap.h
+++
@@ -1,4 +0,0 @@
-#include<stdint.h>
-
-int bitmap_get(uint8_t* bm, int i);
-void bitmap_put(uint8_t* bm, int i, int v);
--- Assignment2/filesystem/slist.h
+++
@@ -1,8 +0,0 @@
-typedef struct slist{
-    char* data;
-    struct slist* next;
-} slist;
-
-slist* s_cons(const char* text, slist* next);
-void s_free(slist* s);
-slist* s_split(const char* text, char delim);
